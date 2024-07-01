50d160aadc09bb74fb2c9b4689e370ca648bd42f
Vertex194 <92236360+Vertex194@users.noreply.github.com>
2024-07-01 19:11:12 +0800

    Add files via upload

---
+++ shop/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from.models import Profile, Order, Cake
+
+class ProfileForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = Profile
+        fields = ['address', 'phone']
+
+class OrderForm(forms.ModelForm):
+    cake = forms.ModelChoiceField(queryset=Cake.objects.all(), empty_label='Select a cake')
+    
+    class Meta:
+        model = Order
+        fields = ['cake', 'quantity']
+
